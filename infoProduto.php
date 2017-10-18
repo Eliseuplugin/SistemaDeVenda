4b5d736c10d7d1e59b7a8f7f304468b153a2131a
Eliseu <eliseuplugin@gmail.com>
2017-10-18 16:38:53 -0600

    ficheiro informacao do produto

---
+++ infoProduto.php
@@ -0,0 +1,20 @@
+<?php
+include './configurar/configServer.php';
+include './configurar/consultSQL.php';
+?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<title>Produtos</title>
+	<?php include './inc/link.php';?>
+</head>
+<body id ="container-page-product">
+	<?php include './inc/navbar.php'; ?>
+	<section id="infoproduto">
+		<div  class="container">
+			<div class="row"></div>
+		</div>
+	</section>
+
+</body>
+</html>
