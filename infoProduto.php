a5efdb7a3ed4e071f0aff1bdc9cdd4b419389072
Eliseu <eliseuplugin@gmail.com>
2017-10-18 16:59:22 -0600

    funcao de mysql_fetch_array

--- infoProduto.php
+++ infoProduto.php
@@ -12,7 +12,24 @@ include './configurar/consultSQL.php';
 	<?php include './inc/navbar.php'; ?>
 	<section id="infoproduto">
 		<div  class="container">
-			<div class="row"></div>
+			<div class="row">
+				<div class="page-header">
+					<h1>Online <small class="tittles-pages-logo">Dep.Vendas</small></h1>
+				</div>
+				<?php 
+					SCodigoProduto=$_GET['CodigoProd'];
+					$produtoinfo= executarSQL::consultar("select * form produto where CodigoProd ='".$CodigoPoduto."
+						'");
+					while ($fila=mysql_fetch_array($produtoinfo)) {
+						echo '
+							<div class="col-xs-12 col-sm-6"> 
+								<h3></h3>
+								<h3></h3>
+								<h3></h3>
+							</div>
+						';
+					}
+			</div>
 		</div>
 	</section>
 
