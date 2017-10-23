ab9fe3f14f770266e4ea740abab92a5af258a3ce
Eliseu <eliseuplugin@gmail.com>
2017-10-23 17:30:17 -0600

    novo formulario de registro

---
+++ registro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<title>Registro</title>
+	<?php include './inc/link.php'; ?>
+</head>
+<body id="container-page-registration">
+		<?php include './inc/navbar.php'; ?>
+		<section id="form-registration">
+			<div class="container">
+				<div class="row">
+					<div class="page-header">
+						<h1>Registro de usuarios <small class="tittles-pages-logo">Dep.Vendas</small></h1>
+					</div>
+					<div class="col-xs-12 col-sm-6 text-center">
+						<br><br><br>
+						<p><i class="fa fa-users fa-5x"></i></p>
+						<p class="lead">
+							Todos os registrados receberao notificacoes de nossos produtos e ofertas mais recentes em nossa loja
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+</body>
+</html>
