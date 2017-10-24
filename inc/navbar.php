d0bda90d138e762efb4c4c629ddfd2bc20981ccb
Eliseu <eliseuplugin@gmail.com>
2017-10-24 16:14:53 -0600

    modelo de login no navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -31,11 +31,100 @@ if (!isset($_SESSION['contador'])) {
 		</div>
 		<div class="col-xs-8">
 			<div class="tabela-conteudo pull-right">
-				<div class="conteudo">
-					
+				<div class="conteudo-tr">
+					<a href="index.php" class="table-cell-td">Inicio</a>
+					<a href="produto.php" class="table-cell-td">Produtos</a>
+					<?php 
+					if (!$_SESSION['nomeAdmin']=="") {
+						echo '
+							<a href="configAdmin.php" class="table-cell-td">Administracao</a>
+							<a href="#" class="table-cell-td carrinho-button-nav all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Ver o carrinho de compras">
+							<i class="fa fa-shopping-cart"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-caret-down"></i>
+							</a>
+							<a href="#" class="table-cell-td" data-toggle="modal" data-target=".modal-logout">
+							<i class="fa fa-user"></i>&nbsp;&nbsp;'.$_SESSION['nomeAdmin'].'
+							</a>
+						';
+					}else if (!$_SESSION['nomeUser']=="") {
+						echo '
+							<a href="pedido.php" class="table-cell-td">Pedido</a>
+							<a href="#" class="table-cell-td carrinho-button-nav all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Ver o carrinho de compras">
+							<i class="fa fa-shopping-cart"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-caret-down"></i>
+							</a>
+							<a href="#" class="table-cell-td" data-toggle="modal" data-target=".modal-logout">
+							<i class="fa fa-user"></i>&nbsp;&nbsp;'.$_SESSION['nomeUser'].'
+							</a>
+						';
+					}else{
+						echo '
+							<a href="registro.php" class="table-cell-td">Registro</a>
+							<a href="#" class="table-cell-td carrinho-button-nav all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Ver o carrinho de compras">
+							<i class="fa fa-shopping-cart"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-caret-down"></i>
+							</a>
+							<a href="#" class="table-cell-td" data-toggle="modal" data-target=".modal-login">
+							<i class="fa fa-user"></i>&nbsp;&nbsp;Login
+							</a>
+						';
+					}
+					?>
 				</div>
 				
 			</div>
 		</div>
 	</div>
+	<div class="row visible-xs">
+		<div class="col-xs-12">
+			<button class="btn btn-default pull-left button-mobile-menu" id="btn-mobile-menu">
+				<i class="fa fa-th-list"></i>&nbsp;&nbsp;Menu
+			</button>
+			<a href="#" id="button-shopping-cart-xs" class="elements-nav-xs all-elements-tooltip carrinho-button-nav" data-toggle="tooltip" data-placement="bottom" title="Ver carrinho de compras">
+				<i class="fa fa-shopping-cart"></i>&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-caret-down"></i>
+			</a>
+			<?php 
+			if (!$_SESSION['nomeAdmin']=="") {echo '
+					<a href="#" id="button-login-xs" class="elements-nav-xs" data-toggle="modal"
+				data-target=".modal-logout">
+				<i class="fa fa-user"</i>&nbsp; '.$_SESSION['nomeAdmin'].'
+				</a>';
+			}else if (!$_SESSION['nomeUser']=="") {
+				echo '
+					<a href="#" id="button-login-xs" class="elements-nav-xs" data-toggle="modal"
+				data-target=".modal-logout">
+				<i class="fa fa-user"</i>&nbsp; '.$_SESSION['nomeUser'].'
+				</a>';
+			}else{
+				echo '
+					<a href="#" id="button-login-xs" class="elements-nav-xs" data-toggle="modal"
+				data-target=".modal-logout">
+				<i class="fa fa-user"</i>&nbsp; Iniciar a sessao
+				</a>
+				';
+				
+			}
+			?>
+		</div>
+	</div>
 </nav>
+
+<!-- Modelo de login -->
+
+<div class="modal fade modal-login" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content" id="modal-form-login">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title text-center text-primary" id="myModalLabel">Iniciar a sessao no Departamento de Vendas</h4>
+			</div>
+			<form action="process/login.php" method="post" role="form" style="margin: 20px;" class="FormDepVendas" data-form="login" >
+				<div class="form-group">
+					<label><span class="glyphicon glyphicon-user"></span>&nbsp;Nome</label>
+					<input type="text" class="form-control" name="nome-login" placeholder="Digite seu nome" required=""/>
+				</div>
+				<div class="form-group">
+					<label><span class="glyphicon glyphicon-lock"></span>&nbsp;Senha</label>
+					<input type="text" class="form-control" name="nome-login" placeholder="Digite sua senha" required=""/>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
