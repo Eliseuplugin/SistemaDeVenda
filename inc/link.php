17ce76c9409d8edd99c09b00c55e7aedd2d8a8ac
Eliseu <eliseuplugin@gmail.com>
2017-10-24 11:14:52 -0600

    script de main e carrinho.js

--- inc/link.php
+++ inc/link.php
@@ -9,5 +9,5 @@
 <script src="js/jquery.min.js" ></script>
 <script src="js/bootstrap.min.js" ></script>
 <script src="js/autohidingnavbar.min.js" ></script>
-<script src="js/" ></script>
-<script src="js/" ></script>
+<script src="js/main.js" ></script>
+<script src="js/carrinho.js" ></script>
